0997f1997cc0a2f8d271fe72c4a33f96c8b39c6d
Richard <codyrwa2011@me.com>
2021-03-09 23:06:07 -0800

    xOr HW
    finished equivalence

--- XOR_Equivalence/main.cpp
+++ XOR_Equivalence/main.cpp
@@ -2,14 +2,38 @@
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    bool p = true, q = false;
-    cout<<"(p&&q) || !(p||q)  =  !(p^q)"<<endl;
-    cout<<((p&&q)||!(p||q))?'T':'F';cout<<endl;
-    cout<<(!(p^q))?'T':'F';
-    return 0;
-}
+void xOr_one(bool, bool);
+void xOr_two(bool, bool);
 
+int main(int argc, char** argv) {
+    cout<<"(p&&q)||~(p||q) = !(p^q)"<<endl;
+    //first row for first table
+    xOr_one(true, true);
+    //second row for first table
+    xOr_one(false, true);
+    //third row for first table
+    xOr_one(true, false);
+    //fourth row for first table
+    xOr_one(false, false);
+//------------------------------------- 
+
+    cout<<"(p||q)&&~(p&&q) = (p^q)"<<endl;
+    //first row for first table
+    xOr_two(true, true);
+    //second row for first table
+    xOr_two(false, true);
+    //third row for first table
+    xOr_two(true, false);
+    //fourth row for first table
+    xOr_two(false, false);
+}
+void xOr_one(bool p, bool q){
+    cout<<"        "<<(((p&&q)||!(p||q))?"T":"F")
+        <<"       |    "
+        <<(!(p^q)?'T':'F')<<endl;
+}
+void xOr_two(bool p, bool q){
+    cout<<"        "<<(((p||q)&&!(p&&q))?"T":"F")
+        <<"       |    "
+        <<((p^q)?'T':'F')<<endl;
+}
